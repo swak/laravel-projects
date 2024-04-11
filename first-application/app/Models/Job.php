a84f806aba7204741edab439c66f4f1216c2db06
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-11 13:19:22 -0700

    Factories in Day 10

--- first-application/app/Models/Job.php
+++ first-application/app/Models/Job.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
+    use HasFactory;
+
     protected $table = 'jobs_listings';
 
     protected $fillable = ['title', 'salary'];
--- first-application/database/factories/UserFactory.php
+++ first-application/database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -41,4 +42,11 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'admin' => true,
+        ]);
+    } // User::factory()->admin()->create()
 }
