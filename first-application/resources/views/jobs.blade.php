dcf1305328a6c50c5051204fe72f4ffdf04c3afb
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-14 13:37:04 -0700

    paginate start with day 14

--- first-application/resources/views/jobs.blade.php
+++ first-application/resources/views/jobs.blade.php
@@ -7,11 +7,16 @@
         @foreach ($jobs as $job)
             <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border-gray-200 rounded-lg">
                 <div class="font-bold text-blue-500">{{ $job->employer->name }}</div>
+
                 <div>
                     <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year.
                 </div>
             </a>
         @endforeach
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
     </div>
 
 </x-layout>
