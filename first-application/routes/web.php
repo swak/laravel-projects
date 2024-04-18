72b1b438cd7078c3d762a55ff4956f2e2a2b31e4
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-18 13:29:03 -0700

    validation

--- first-application/routes/web.php
+++ first-application/routes/web.php
@@ -8,17 +8,38 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(5);
+    $jobs = Job::with('employer')->latest()->simplePaginate(5);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function() {
+    // dd(request()->all());
+    // validation...
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
