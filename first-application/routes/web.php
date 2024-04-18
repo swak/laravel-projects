e0d1ac0910cf429771e22131ee1169f3c42d519c
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-18 14:46:16 -0700

    completed crud for delete and edit, day 18 complete

--- first-application/routes/web.php
+++ first-application/routes/web.php
@@ -46,12 +46,42 @@ Route::post('/jobs', function() {
     return redirect('/jobs');
 });
 
+// Edit
 Route::get('/jobs/{id}/edit', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.edit', ['job' => $job]);
 });
 
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+    // authorize (on hold)
+    // update job
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+    // and persist
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (on hold)
+    // delete the job
+    $job = Job::findOrFail($id);
+    $job->delete();
+    // redirect
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function () {
     return view('contact');
