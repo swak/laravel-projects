f71c26f3f4a95c517f8d2e51e3d948e259eab5d6
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-19 15:48:06 -0700

    Route Grouping format

--- first-application/routes/web.php
+++ first-application/routes/web.php
@@ -6,14 +6,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 
 Route::view('/contact', 'contact');
