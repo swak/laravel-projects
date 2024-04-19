5076d850b531c548749812048823b6bbb7024f16
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-19 15:42:22 -0700

    Update Route View which is used for static pages usually.

--- first-application/routes/web.php
+++ first-application/routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Route::view('/', 'home');
 
 
 Route::get('/jobs', [JobController::class, 'index']);
@@ -17,6 +16,4 @@ Route::patch('/jobs/{job}', [JobController::class, 'update']);
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
