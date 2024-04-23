5d98091c85404a05bd643a1dc928108ae7cce2a0
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-23 13:00:55 -0700

    finished register and login controllers.

--- first-application/app/Http/Controllers/RegisteredUserController.php
+++ first-application/app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +16,23 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $validatedAttributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email', 'max:254'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+
+        // dd($validatedAttributes);
+
+        // create the user
+        $user = User::create($validatedAttributes);
+
+        // log in
+        Auth::login($user);
+
+        // redirect somewhere
+        return redirect('/jobs');
     }
 }
