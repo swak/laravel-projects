bfd47661e15a1bdd06630fc12b0492eab834f1f4
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-25 15:08:06 -0700

    using policies instead of gate

--- first-application/resources/views/jobs/show.blade.php
+++ first-application/resources/views/jobs/show.blade.php
@@ -9,7 +9,7 @@
         This job pays {{ $job->salary }} per year.
     </p>
 
-    @can('edit-job', $job)
+    @can('edit', $job)
     <p class="mt-6">
         <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
     </p>
