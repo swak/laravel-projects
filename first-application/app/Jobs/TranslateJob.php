f29ef52041f523d893cf9ee9464281bf43145924
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-27 12:45:06 -0700

    creating queue services with workers

---
+++ first-application/app/Jobs/TranslateJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Job;
+
+class TranslateJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Job $jobListing)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // AI::translate($this->jobListing->description, 'spanish');
+        logger('Translating ' . $this->jobListing->title . ' to Spanish.');
+    }
+}
