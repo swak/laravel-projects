fae594bb249f415658a3f6197cf6f79e2cb2a7a1
Shaun Wakashige <shaun.wakashige@gmail.com>
2024-04-27 22:09:05 -0700

    starting integration using vite and loading tailwindcss locally

--- first-application/resources/views/components/layout.blade.php
+++ first-application/resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
         <title>Home Page</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite(['resources/js/app.js','resources/css/app.css'])
     </head>
     <body class="h-full">
 
